3404801a7c153fb33dcff9e5746330b14216341c
YangSiJun528 <yangsijun5528@gmail.com>
2023-12-01 10:38:48 +0900

    project init

---
+++ src/main/java/dev/yangsijun/rehello/ReHelloApplication.java
@@ -0,0 +1,13 @@
+package dev.yangsijun.rehello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReHelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/yangsijun/rehello/ReHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yangsijun.rehello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
